945d45823e5876884260131b4755bcb1bf92fe77
Andreas <achristianson@sourceallies.com>
2019-03-18 00:15:58 -0500

    echo. ECHO

---
+++ echo/src/index.js
@@ -0,0 +1,14 @@
+import aws from 'aws-sdk';
+
+exports.handler = async (event, context) => {
+    const postData = JSON.parse(event.body).data;
+  
+
+    return { 
+        statusCode: 200, 
+        body: {
+            echo: true,
+            ...postData
+        } 
+    };
+};
