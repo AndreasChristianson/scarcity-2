9a9342186aaae11b86d02b0bb8087fe2c3915546
Andreas <achristianson@sourceallies.com>
2019-03-23 13:44:48 -0500

    set preset to target node 8

--- push.sh
+++ push.sh
@@ -8,4 +8,5 @@ zip -r -X app.zip .
 cd -
 digest=$(openssl dgst -r echo/dist/app.zip)
 aws s3 cp echo/dist/app.zip s3://scarcity-artifacts/echo-$(git rev-parse --short HEAD).zip
+
 cloudformation/deploy.sh $1
