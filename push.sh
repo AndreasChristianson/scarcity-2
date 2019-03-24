2dc21bf58a697ace00350b44e2ec9b64c3a88f33
Andreas <achristianson@sourceallies.com>
2019-03-23 20:06:01 -0500

    setting up node_module pack

--- push.sh
+++ push.sh
@@ -2,9 +2,15 @@
 
 set -e -o xtrace
 
+npm --prefix echo install
 npm --prefix echo run build
+rm -rf echo/node_modules
+npm --prefix echo install --production
 cd echo/dist
 zip -r -X app.zip .
+cd -
+cd echo
+zip -rqX dist/app.zip node_modules
 cd -
 digest=$(openssl dgst -r echo/dist/app.zip)
 aws s3 cp echo/dist/app.zip s3://scarcity-artifacts/echo-$(git rev-parse --short HEAD).zip
