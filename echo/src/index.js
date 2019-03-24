808160fe235390f043ca97c846665b1fbe2a3b1c
Andreas <achristianson@sourceallies.com>
2019-03-23 19:45:54 -0500

    echo might work...

--- echo/src/index.js
+++ echo/src/index.js
@@ -1,16 +1,20 @@
-import aws from 'aws-sdk';
+import AWS from 'aws-sdk';
 
 exports.handler = async (event, context) => {
-    const postData = JSON.parse(event.body).data;
+    const postData = JSON.parse(event.body);
 
-    console.info({event})
+    const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: '2018-11-29',
+        endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+    });
 
-    return { 
-        statusCode: 200, 
+    const connectionId = event.requestContext.connectionId;
+
+    await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+
+    return {
+        statusCode: 200,
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            echo: true,
-            ...postData
-        })
+        body: `Echo: ${postData}`
     };
 };
