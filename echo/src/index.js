359c53cb530807bb3847993019c39b384e478f13
Andreas <achristianson@sourceallies.com>
2019-03-23 19:11:50 -0500

    adding verbose loggin

--- echo/src/index.js
+++ echo/src/index.js
@@ -3,12 +3,14 @@ import aws from 'aws-sdk';
 exports.handler = async (event, context) => {
     const postData = JSON.parse(event.body).data;
 
+    console.info({event})
+
     return { 
         statusCode: 200, 
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
+        body: {
             echo: true,
             ...postData
-        }) 
+        }
     };
 };
