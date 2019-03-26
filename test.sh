a213b2978d4314a8960cfb85e64b8ca74650ed9d
Andreas <achristianson@sourceallies.com>
2019-03-25 23:04:32 -0500

    working on execute-api permissions

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -e -o xtrace
 
 url=$(aws cloudformation describe-stacks  --stack-name scarcity-$1 | jq --raw-output '.Stacks[0].Outputs[] | select(.OutputKey == "WebSocketURI") | .OutputValue')
-response=$(wscat --execute "{\"action\":\"echo\",\"echo\":\"$TRAVIS_JOB_NUMBER\"}" --connect $url)
+response=$(wscat --execute "{\"action\":\"echo\",\"echo\":\"$TRAVIS_JOB_NUMBER\"}" --connect $url -w 10)
 if [[ ! $response =~ $TRAVIS_JOB_NUMBER ]]; then 
     exit 1; 
 fi
