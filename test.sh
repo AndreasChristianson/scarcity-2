d427d2b4faf5f89c74cd725c2397d97aa5d95d07
Andreas <achristianson@sourceallies.com>
2019-03-26 21:21:02 -0500

    updating paths

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@
 
 set -e -o xtrace
 
+uuid=$(uuidgen)
 url=$(aws cloudformation describe-stacks  --stack-name scarcity-$1 | jq --raw-output '.Stacks[0].Outputs[] | select(.OutputKey == "WebSocketURI") | .OutputValue')
-response=$(wscat --execute "{\"action\":\"echo\",\"echo\":\"$TRAVIS_JOB_NUMBER\"}" --connect $url -w 10)
-if [[ ! $response =~ $TRAVIS_JOB_NUMBER ]]; then 
+response=$(wscat --execute "{\"action\":\"echo\",\"echo\":\"$uuid\"}" --connect $url -w 10)
+if [[ ! $response =~ $uuid ]]; then 
     exit 1; 
 fi
