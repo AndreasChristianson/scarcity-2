5f79cdf88badec4a40e83ffb98f58684d8ef8080
Andreas Christianson <achristianson@hy-vee.com>
2019-04-01 12:59:48 -0500

    added six hour connection ttl

--- packages/wss/connect/src/index.js
+++ packages/wss/connect/src/index.js
@@ -4,12 +4,19 @@ const ddb = new AWS.DynamoDB({
     apiVersion: '2012-08-10'
 });
 
+const sixHoursFromNow = () => Math.floor(Date.now() / 1000) + 60 * 60 * 6;
+
 exports.handler = async (event, context) => {
     const { requestContext: { connectionId } } = event;
     const putParams = {
         TableName: process.env.TABLE_NAME,
         Item: {
-            connectionId: { S: connectionId }
+            connectionId: {
+                S: connectionId 
+            },
+            ttl: {
+                N: sixHoursFromNow() 
+            }
         }
     };
 
