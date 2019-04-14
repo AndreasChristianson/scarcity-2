ccdb13b21f4b543e047eae13ea2d18722b09bcf2
Andreas <achristianson@sourceallies.com>
2019-04-14 16:27:21 -0500

    gave up on having cloudfront host a path for wss

--- site/src/services/wss/get-wss-url.js
+++ site/src/services/wss/get-wss-url.js
@@ -1,15 +1,1 @@
-const URL_FILE_PATH = '/wss-url';
-const fetchWssUrl = async () => {
-    const response = await fetch(URL_FILE_PATH);
-    const json = await response.json();
-    return json
-};
-
-let urlPromise;
-
-export const getWssUrl = () => {
-    if(!urlPromise){
-        urlPromise = fetchWssUrl();
-    }
-    return urlPromise;
-}
+export const getWssUrl = () => `wss://wss.${window.location.host}`;
