43ab08edc7ec5f13f177fd06f53545d678d1c8ed
Andreas <achristianson@sourceallies.com>
2019-04-20 23:12:31 -0500

    getting scarcity state off the ground

--- site/src/redux/middleware/websocket.js
+++ site/src/redux/middleware/websocket.js
@@ -1,23 +1,52 @@
 import getWssUrl from '../../services/wss/get-wss-url';
 import { LOG, SCARCITY_UPDATE, WS_CLOSED, WS_READY } from '../action-types';
 
+const assert = (value, validValues) =>{
+  if(!value){
+    throw new Error(`Empty value: ${value}`);
+  }
+  if(validValues && !validValues.includes(value)){
+    throw new Error(`Illegal value: ${value} (legal values are ${validValues})`);
+  }
+}
+
 const websocketMiddleware = store => {
   const websocket = new WebSocket(getWssUrl());
 
-  websocket.onmessage = ({ data }) => {
-    const payload = JSON.parse(data)
+  const log = (message, details) => {
+    store.dispatch({
+      type: LOG,
+      message,
+      details
+    });
+  };
 
-    if (payload.criteria) {
+  websocket.onmessage = async ({ data }) => {
+    try {
+      const { criteria, ...fields } = JSON.parse(data)
+      const { table, id, action } = criteria;
+
+      assert(table, [
+        'floor',
+        'character',
+        'chat',
+        'actions'
+      ]);      
+      assert(id);
+      assert(action, [
+        'add',
+        'remove',
+        'update',
+        'replace'
+      ]);
+
       store.dispatch({
-        type: SCARCITY_UPDATE,
-        ...payload
-      })
-    } else {
-      store.dispatch({
-        type: LOG,
-        message: 'Unexpected message shape.',
-        details: payload
-      })
+        ...fields,
+        type: `${SCARCITY_UPDATE}-${criteria.table}`,
+        criteria
+      });
+    } catch (error) {
+      log('Invalid websocket message', { error, data });
     }
   };
 
@@ -26,11 +55,8 @@ const websocketMiddleware = store => {
     ...closeEvent
   });
 
-  websocket.onerror = (event) => store.dispatch({
-    type: LOG,
-    message: 'Unexpected websocket event.',
-    details: event
-  });
+  websocket.onerror = (event) =>
+    log('Unexpected websocket event.', event);
 
   websocket.onopen = (event) => store.dispatch({
     type: WS_READY,
--- site/src/redux/reducers/scarcity-reducer.js
+++ site/src/redux/reducers/scarcity-reducer.js
@@ -1,12 +1,38 @@
+import { combineReducers } from 'redux';
+import merge from 'merge-deep';
+
 import { SCARCITY_UPDATE } from '../action-types';
 
-const messageReducer = (state = {}, { type, criteria, fields }) => {
-  switch (type) {
-    case SCARCITY_UPDATE:
-      return applyStateUpdate(state, criteria, fields)
-    default:
-      return state
+const applyStateUpdate = (map, {action, id}, data) => {
+  switch (action) {
+    case 'replace':
+    case 'add':
+      return map.set(id, data);
+    case 'remove':
+      map.delete(id);
+      return map;
+    case 'update':
+      const newValue = merge(map.get(id), data);
+      return map.set(id, newValue);
   }
-}
+};
 
-export default messageReducer;
+const createScarcityReducer = (table) => (state = new Map(), { type, criteria, ...data }) => {
+  if (type === `${SCARCITY_UPDATE}-${table}`) {
+    return applyStateUpdate(state, criteria, data);
+  } else {
+    return state;
+  }
+};
+
+const reducers = [
+  'floor',
+  'character',
+  'chat',
+  'actions'
+].reduce((prev, curr) => ({
+  ...prev,
+  [curr]: createScarcityReducer(curr)
+}), {});
+
+export default combineReducers(reducers);
