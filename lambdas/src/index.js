24f43a19d93f5b73e2d7bb5fc79af6aaa49d225b
Andreas Christianson <sullage@pessimistic-it.com>
2019-07-15 23:39:42 -0500

    setting up a logger for the default route

--- lambdas/src/index.js
+++ lambdas/src/index.js
@@ -1,3 +1,4 @@
 export {handler as connect} from './connect';
 export {handler as echo} from './echo';
 export {handler as disconnect} from './disconnect';
+export {handler as logger} from './logger';
---
+++ lambdas/src/logger.js
@@ -0,0 +1,14 @@
+import AWS from 'aws-sdk';
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+export const handler = async (event, context) => {
+    const request = JSON.parse(event.body);
+    console.log('Bad request', {request, context};
+
+    return {
+        statusCode: 400,
+        headers: { 'Content-Type': 'application/json' },
+        body: `Bad request: ${request}`
+    };
+};
