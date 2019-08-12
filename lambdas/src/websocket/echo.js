9d8b7e32eba7a301d0edb9c983e4887958d29aae
Andreas Christianson <sullage@pessimistic-it.com>
2019-08-12 01:33:41 -0500

    removed dead code

--- lambdas/src/websocket/echo.js
+++ lambdas/src/websocket/echo.js
@@ -1,15 +1,9 @@
-import AWS from 'aws-sdk';
 import {accessDDb} from '../util/ddb-crud';
 import {postToConnection} from '../util/connections';
 
 export const handler = async (event, context) => {
     const { echo = 'hello world!' } = JSON.parse(event.body);
     const postData = JSON.stringify(echo);
-
-    const managementApi = new AWS.ApiGatewayManagementApi({
-        apiVersion: '2018-11-29',
-        endpoint: `${event.requestContext.apiId}.execute-api.${process.env['AWS_REGION']}.amazonaws.com/${event.requestContext.stage}`
-    });
 
     const scanParams = {
         TableName: 'Connections',
