46839a22cbcbaab945fce2f6113f9a6ebb63593e
Andreas Christianson <sullage@pessimistic-it.com>
2019-08-12 23:22:27 -0500

    commenttingout ideas

--- lambdas/src/events/handle-floor-object-changes.js
+++ lambdas/src/events/handle-floor-object-changes.js
@@ -1,7 +1,7 @@
-import getObservers from '../util/connections';
+// import getObservers from '../util/connections';
 
 export const handler = async ({Records}, context, callback) =>
     Records.forEach(({dynamodb:{OldImage, NewImage, SequenceNumber, Keys}, ...event}) => {
         const {objectId} = NewImage;
-        const observers = await getObservers();
+        // const observers = await getObservers();
     });
